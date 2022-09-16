46b2084404ee3c4e121c0ed8bcd7837db50ee7a2
Valter Vianês <valter.vianes@gmail.com>
2022-09-16 21:34:26 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace SyntraWestAdvancedDotNetEventsExercise2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.cbRed = new System.Windows.Forms.CheckBox();
+            this.cbGreen = new System.Windows.Forms.CheckBox();
+            this.cbBlue = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(112, 68);
+            this.trackBar1.Maximum = 9;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(500, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(112, 178);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(150, 150);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStop.Location = new System.Drawing.Point(462, 178);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(150, 150);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // cbRed
+            // 
+            this.cbRed.AutoSize = true;
+            this.cbRed.Checked = true;
+            this.cbRed.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbRed.Location = new System.Drawing.Point(158, 428);
+            this.cbRed.Name = "cbRed";
+            this.cbRed.Size = new System.Drawing.Size(46, 19);
+            this.cbRed.TabIndex = 3;
+            this.cbRed.Text = "Red";
+            this.cbRed.UseVisualStyleBackColor = true;
+            // 
+            // cbGreen
+            // 
+            this.cbGreen.AutoSize = true;
+            this.cbGreen.Checked = true;
+            this.cbGreen.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbGreen.Location = new System.Drawing.Point(329, 428);
+            this.cbGreen.Name = "cbGreen";
+            this.cbGreen.Size = new System.Drawing.Size(57, 19);
+            this.cbGreen.TabIndex = 4;
+            this.cbGreen.Text = "Green";
+            this.cbGreen.UseVisualStyleBackColor = true;
+            // 
+            // cbBlue
+            // 
+            this.cbBlue.AutoSize = true;
+            this.cbBlue.Checked = true;
+            this.cbBlue.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbBlue.Location = new System.Drawing.Point(511, 428);
+            this.cbBlue.Name = "cbBlue";
+            this.cbBlue.Size = new System.Drawing.Size(49, 19);
+            this.cbBlue.TabIndex = 5;
+            this.cbBlue.Text = "Blue";
+            this.cbBlue.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 497);
+            this.Controls.Add(this.cbBlue);
+            this.Controls.Add(this.cbGreen);
+            this.Controls.Add(this.cbRed);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.CheckBox cbRed;
+        private System.Windows.Forms.CheckBox cbGreen;
+        private System.Windows.Forms.CheckBox cbBlue;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SyntraWestAdvancedDotNetEventsExercise2
+{
+    public partial class Form1 : Form
+    {
+        private Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int redColor = CheckColorStatus(cbRed);
+            int greenColor = CheckColorStatus(cbGreen);
+            int blueColor = CheckColorStatus(cbBlue);
+
+            this.BackColor = Color.FromArgb(redColor, greenColor, blueColor);
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if (trackBar1.Value == 0)
+            {
+                timer1.Interval = 1000;
+            }
+            else
+            {
+                timer1.Interval = 1000 / trackBar1.Value;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private int CheckColorStatus(CheckBox aCheckbox)
+        {
+            if(aCheckbox.Checked)
+            {
+                return _random.Next(256);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
